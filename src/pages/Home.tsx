7965854bff1d0b985a3c9bfb764dcd3162e4b950
ANZAR <anzarkhan790@gmail.com>
2025-01-21 17:20:05 +0530

    typescript errors resolved

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import{ useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { getRecipes } from "../lib/api";
 import { Recipe } from "../interfaces/RecipeInterface";
--- src/pages/RecipeDetails.tsx
+++ src/pages/RecipeDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import{ useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { getRecipeDetails } from "../lib/api";
 import { Recipe } from "../interfaces/RecipeInterface";
